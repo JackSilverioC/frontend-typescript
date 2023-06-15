74f7196f6aee84edae97b859a2161e6130e29121
Jack Silverio <silveriocieza.jackanibal@gmail.com>
2023-06-15 15:58:34 -0500

    fixing

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-export default {
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   base: "/frontend-typescript/"
-};
+});
